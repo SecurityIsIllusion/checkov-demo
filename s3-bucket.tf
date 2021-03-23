303c0ecb020d72e0339831eda33854db9f06d150
Rohit Ranjan <rrohitrockss@gmail.com>
2021-03-23 09:40:43 +0530

    Create s3-bucket.tf

---
+++ s3-bucket.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket" "foo-bucket" {
+checkov:skip=CKV_AWS_20:Skip Test for checking
+  region        = var.region
+  bucket        = local.bucket_name
+  force_destroy = true
+
+  tags = {
+    Name = "foo-${data.aws_caller_identity.current.account_id}"
+  }
+  versioning {
+    mfa_delete = true
+  }
+  logging {
+    target_bucket = "${aws_s3_bucket.log_bucket.id}"
+    target_prefix = "log/"
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.mykey.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  acl = "public-read"
+}
+data "aws_caller_identity" "current" {}
